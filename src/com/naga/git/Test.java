4d21009d4f2d68580713a0b216a3ab52e9f239c8
thota <thota@DESKTOP-3RFTA5P>
2020-07-23 20:42:26 +0530

    WH-5211 - update

--- src/com/naga/git/Test.java
+++ src/com/naga/git/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		EmployeeDetails ed=new EmployeeDetails();
+		ed.setEid(10);
+		ed.setEname("Naga");
+		ed.setEsal(20.0f);
 		System.out.println(ed.getEid()+","+ed.getEname()+","+ed.getEsal());
 
 	}
