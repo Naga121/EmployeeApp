3cc6114b84bdceeb3a6f2ac8b64e979600e986ea
thota <thota@DESKTOP-3RFTA5P>
2020-07-23 20:34:35 +0530

    WH-5555 - First time file update

---
+++ src/com/naga/git/Test.java
@@ -0,0 +1,11 @@
+package com.naga.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		EmployeeDetails ed=new EmployeeDetails();
+		System.out.println(ed.getEid()+","+ed.getEname()+","+ed.getEsal());
+
+	}
+
+}
